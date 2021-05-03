23152c87d63fadac93b05e3420b60b3743bea2b0
ruchitabaglane <ruchitabaglane@gmail.com>
2021-05-03 00:15:15 -0500

    Modified code for visualisation and added a few comments.

--- final_project.py
+++ final_project.py
@@ -40,15 +40,16 @@ def calcVaccinationRate(Total_Individuals_Vaccinated: float, Population_2019: fl
 
 
 def calcRateOfVaccination(vaccineRecords: pd.DataFrame, statePop: pd.DataFrame,
-                          stateGovernments: pd.DataFrame) -> pd.DataFrame:
+                          stateGovernments: pd.DataFrame) :
     vaccinePopulation = statePop[['State', 'Population_2019']].merge(
-        vaccineRecords[['Updated On', 'State', 'Total Individuals Vaccinated']], on='State')
+                        vaccineRecords[['Updated On', 'State', 'Total Individuals Vaccinated']], on='State')
     vaccinePopulation['VaccinationRate'] = vaccinePopulation.apply(
         lambda row: calcVaccinationRate(row['Total Individuals Vaccinated'], row['Population_2019']), axis=1)
 
     vaccinePopulation1 = statePop[['State', 'Population_2019']].merge(
-        vaccineRecords[['Updated On', 'State', 'Total Individuals Vaccinated']], on='State').merge(
-        stateGovernments[['State', 'Party']], on='State')
+                         vaccineRecords[['Updated On', 'State', 'Total Individuals Vaccinated']], on='State').merge(
+                         stateGovernments[['State', 'Party']], on='State')
+
     grouped = vaccinePopulation1.groupby('Party').agg({'Population_2019': 'sum', 'Total Individuals Vaccinated': 'sum'})
     grouped = grouped.reset_index()
     grouped['VaccinationRate'] = grouped.apply(
@@ -64,27 +65,45 @@ def StateClassificationByRulingParty(stateGovernments: pd.DataFrame, rateOfVacci
 
 
 if __name__ == '__main__':
+    # Loading all the input files
     stateElections = pd.read_csv("./StateElectionData.csv")
     stateVaccinations = pd.read_csv("./covid_vaccine_statewise.csv")
     statePopulation = pd.read_csv("./statePopulationIndia.csv")
+    # CLeaning the data to have uniform names of states across the input files.
     statePopulation = cleanData(statePopulation)
 
+    # Fetching the currently ruling parties in each state of India.
     stateGovernments = getStateGovernments(stateElections)
+    # Fetching the statewise vaccination records for the latest dates.
     stateVaccineRecords = getStateVaccineRecords(stateVaccinations)
 
+    # Calculating the rate of vaccination in each state.
     rateOfVaccination_vs_party = calcRateOfVaccination(stateVaccineRecords, statePopulation, stateGovernments)
 
-    # print (rateOfVaccination_vs_party[1])
-
+    # Plotting bar plot for States within each party vs vaccination rate.
     rateOfVaccination_vs_party[0].plot(x='Party', y='VaccinationRate', kind='bar')
+    plt.title(label="Vaccination Rates per Political Party",
+              fontsize=15)
+    plt.xlabel('Political Parties')
+    plt.ylabel('Vaccination Rates')
     plt.show()
 
+    # Classifying the states by ruling parties.
     vaccinationRate_by_rulingParty = StateClassificationByRulingParty(stateGovernments, rateOfVaccination_vs_party[1])
 
+    # plotting bar plot for States vs Vaccination rate and coloring the bar plot to represent the ruling party.
     fig, ax = plt.subplots(figsize=(100, 30))
     for key, grp in vaccinationRate_by_rulingParty.groupby(['Party']):
         ax.bar(grp['State'], grp['VaccinationRate'], label=key)
-    ax.legend()
+
+    for label in (ax.get_xticklabels() + ax.get_yticklabels()):
+        label.set_fontsize(55)
+    ax.legend(loc=2, prop={'size': 40})
+    ax.tick_params(axis='x', labelrotation=90)
+    plt.title(label="States Vs Vaccination Rates",
+              fontsize=95)
+    plt.xlabel('States', fontsize=55)
+    plt.ylabel('Vaccination Rates', fontsize=55)
     plt.show()
 
 
